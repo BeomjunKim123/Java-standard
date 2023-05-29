25d30460d787405a315a5abb6ff92acbbaaf0ce4
BeomjunKim123 <gkgk9753@gmail.com>
2023-05-30 00:06:52 +0900

    2023.05.29 자바의정석_제어자

---
+++ MyParentTest2.java
@@ -0,0 +1,15 @@
+//package 자바의정석;
+//
+//class MyChild extends MyParent {
+//    public void printMembers() {
+////        System.out.println(prv);
+//        System.out.println(dft);
+//        System.out.println(prt);
+//        System.out.println(pub);
+//}
+//
+//public class MyParentTest2 {
+//    public static void main(String[] args) {
+//
+//    }
+//}
